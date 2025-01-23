52054c6ae1dfee9d12253e7d009cfed9778a7eb5
jones58 <108466184+jones58@users.noreply.github.com>
2025-01-23 18:21:04 +0000

    improve position of theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,11 +53,13 @@ export default function Home() {
 
   return (
     <div className="min-h-screen p-4 sm:p-6 lg:p-8">
-      <ThemeToggle />
       <main className="max-w-7xl mx-auto pt-16">
-        <h1 className="text-3xl font-bold mb-8 text-center sm:text-left text-purple-700 dark:text-purple-600">
-          Minecraft Server List
-        </h1>
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold text-purple-700 dark:text-purple-600">
+            Minecraft Server List
+          </h1>
+          <ThemeToggle />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {serverData ? (
             serverData.map((server) => (
